6888978db3ecfde7d3a94c54f200eb0d920a20b8
greg <greg.nwosu@gmail.com>
2017-02-08 15:33:16 +0000

    initial import

---
+++ projects.clj
@@ -0,0 +1,8 @@
+(defproject trivia "0.0.1-SNAPSHOT"
+  :description "Trivia Game"
+  :url "heeebo.com"
+  :license {:name "GPLv3" :url "http://choosealicense.com/license/gpl-3.0/#"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.9.227"]]
+  :plugins [[lein-cljsbuild "1.1.3"]]
+  )
