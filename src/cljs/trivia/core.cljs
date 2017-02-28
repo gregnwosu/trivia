83db04f3de5027b81eb16f2d3ec2d54c106b3586
greg <greg.nwosu@gmail.com>
2017-03-01 00:03:49 +0800

    latest code

--- src/cljs/trivia/core.cljs
+++ src/cljs/trivia/core.cljs
@@ -41,55 +41,8 @@
    [:input.btn-primary {:type "button" :value "Click me Boss!"
                         :on-click #(swap! click-count inc)}]])
 
-;; use clojure multimethods to override pages
-;; from https://github.com/Day8/re-frame/blob/master/docs/Navigation.md
-
-;; setup multimethods, override function called 'pages'
-(defmulti pages identity)
-;; define your pages         views/login-panel from views.cljs
-(defmethod pages :login []
-  [(views/login-panel)])
-(defmethod pages :create-game []
-  (console.log "Rendering create-game")
-
-
-  [:div
-   [:form.form-horizontal
-    [:fieldset
-     [:comment " Form Name "]
-     [:legend "Form Name"]
-     [:comment " Appended checkbox "]
-
-     [:div.form-group
-      [:label.col-md-4.control-label {:for "Greg"} "Appended Checkbox"]
-      [:div.col-md-4
-       [:div.input-group
-        [:input#Greg.form-control {:name "Greg" :type "text" :placeholder "placeholder"}]
-        [:span.input-group-addon [:input {:type "checkbox"}]]]
-       [:p.help-block "help"]]]
-
-     [:comment " Button "]
-     [:div.form-group
-      [:label.col-md-4.control-label {:for "singlebutton"} "Single Button"]
-      [:div.col-md-4
-       [:button#singlebutton.btn.btn-primary {:name "singlebutton"} "Button"]]]]]]
-
-  )
-
-
-;; so now the function show-page takes a page name and dispatches on the multimethod pages above
-(defn show-page [page-name]
-  (prn "Show page" page-name)
-  (pages page-name))
-
-(defn main-page []
-  (let [active-page  (re-frame/subscribe [:active-page])]
-    (fn [] [:div
-           (show-page @active-page)])))
-;;; end multimethod technique for page dispatch
-
 (defn mount-root []
-  (reagent/render [main-page]
+  (reagent/render [views/main-page]
                   ;;[:h1 "Hello"]
                   (.getElementById js/document "app")))
 
--- src/cljs/trivia/views.cljs
+++ src/cljs/trivia/views.cljs
@@ -1,6 +1,7 @@
 (ns trivia.views
-  (:require [reagent.core :as reagent]
-            [re-frame.core :refer [dispatch subscribe]]))
+  (:require [re-frame.core :as re-frame :refer [dispatch subscribe]]
+            [reagent.core :as reagent]))
+
 ;; http://bootsnipp.com/
 ;; boiler plate from http://www.initializr.com/
 ;; configure and download
@@ -12,8 +13,93 @@
   (let [name  (reagent/atom "")]
     (fn []
       [:div.container [:div.row [:div.main [:h3 "Please Log In, or " [:a {:href "#"} "Sign Up"]] [:div.row [:div.col-xs-6.col-sm-6.col-md-6 [:a.btn.btn-lg.btn-primary.btn-block {:href "#"} "Facebook"]] [:div.col-xs-6.col-sm-6.col-md-6 [:a.btn.btn-lg.btn-info.btn-block {:href "#"} "Google"]]] [:div.login-or [:hr.hr-or] [:span.span-or "or"]] [:form {:role "form"} [:div.form-group [:label {:for "inputUsernameEmail"} "Username or email"] [:input#inputUsernameEmail.form-control {:type "text"  :on-change #(reset! name (-> % .-target .-value))}]] [:div.form-group [:a.pull-right {:href "#"} "Forgot password?"] [:label {:for "inputPassword"} "Password"] [:input#inputPassword.form-control {:type "password"}]] [:div.checkbox.pull-right [:label [:input {:type "checkbox"}] "
-  Remember me "]] [:button.btn.btn.btn-primary {:type "button" :on-click #(dispatch [:login-success @name])}
+  Remember me "]] [:button.btn.btn.btn-primary {:type "button" :on-click #(dispatch [:login @name])}
                    "Log In"]]]]])))
+
+
+(defn navbar
+  ""
+  []
+  (let [name (re-frame/subscribe [:name]) ]
+    [:div.container [:comment " Static navbar "] [:nav.navbar.navbar-default [:div.container-fluid [:div.navbar-header [:button.navbar-toggle.collapsed {:type "button" :data-toggle "collapse" :data-target "#navbar" :aria-expanded "false" :aria-controls "navbar"} [:span.sr-only "Toggle navigation"] [:span.icon-bar] [:span.icon-bar] [:span.icon-bar]] [:a.navbar-brand {:href "#"} @name]] [:div#navbar.navbar-collapse.collapse [:ul.nav.navbar-nav [:li.active [:a {:href "#"} "Home"]] [:li [:a {:href "#"} "About"]] [:li [:a {:href "#"} "Contact"]] [:li.dropdown [:a.dropdown-toggle {:href "#" :data-toggle "dropdown" :role "button" :aria-haspopup "true" :aria-expanded "false"} "Dropdown " [:span.caret]] [:ul.dropdown-menu [:li [:a {:href "#"} "Action"]] [:li [:a {:href "#"} "Another action"]] [:li [:a {:href "#"} "Something else here"]] [:li.divider {:role "separator"}] [:li.dropdown-header "Nav header"] [:li [:a {:href "#"} "Separated link"]] [:li [:a {:href "#"} "One more separated link"]]]]] [:ul.nav.navbar-nav.navbar-right [:li.active [:a {:href "./"} "Default " [:span.sr-only "(current)"]]] [:li [:a {:href "../navbar-static-top/"} "Static top"]] [:li [:a {:href "../navbar-fixed-top/"} "Fixed top"]]]] ] ]]))
+
+(defn create-game
+  ""
+  []
+  (fn []
+    [:div
+     (navbar)
+     [:div {:class "container"}
+      [:div {:class "row"}
+       [:div {:class "jumbotron"}
+        [:div {:class "container"}
+         [:h1 "Trivia Game!"]
+         [:p "The most exciting trivia game"]
+         [:p [:a {:class "btn btn-primary btn-lg", :href "#", :role "button"} "Create a new game>>"]]]
+        ]]
+      ]]
+    )
+
+  )
+
+
+
+
+
+
+
+
+(defn create-game-menu
+  ""
+  []
+  (let [mythis "that" ]
+    (fn []
+      [:div
+       [:form.form-horizontal
+        [:fieldset
+         [:comment " Form Name "]
+         [:legend "Form Name"]
+         [:comment " Appended checkbox "]
+
+         [:div.form-group
+          [:label.col-md-4.control-label {:for "Greg"} "Appended Checkbox"]
+          [:div.col-md-4
+           [:div.input-group
+            [:input#Greg.form-control {:name "Greg" :type "text" :placeholder "placeholder"}]
+            [:span.input-group-addon [:input {:type "checkbox"}]]]
+           [:p.help-block "help"]]]
+
+         [:comment " Button "]
+         [:div.form-group
+          [:label.col-md-4.control-label {:for "singlebutton"} "Single Button"]
+          [:div.col-md-4
+           [:button#singlebutton.btn.btn-primary {:name "singlebutton"} "Button"]]]]]])))
+
+
+
+
+;; use clojure multimethods to override pages
+;; from https://github.com/Day8/re-frame/blob/master/docs/Navigation.md
+
+;; setup multimethods, override function called 'pages'
+(defmulti pages identity)
+;; define your pages         views/login-panel from views.cljs
+(defmethod pages :login []
+  [(login-panel)])
+(defmethod pages :create-game []
+  (console.log "Rendering create-game")
+  [(create-game)])
+
+;; so now the function show-page takes a page name and dispatches on the multimethod pages above
+(defn show-page [page-name]
+  (prn "Show page" page-name)
+  (pages page-name))
+
+(defn main-page []
+  (let [active-page  (re-frame/subscribe [:active-page])]
+    (fn [] [:div
+           (show-page @active-page)])))
+;;; end multimethod technique for page dispatch
 
 ;; its the on-click dispatch that makes it fire an event,
 ;; name is subscribed using this lambda syntax
